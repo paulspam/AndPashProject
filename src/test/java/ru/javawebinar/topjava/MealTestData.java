9039342a8ce600f01a383c7fe1dd7a7943207968
paulspam <paulspam@ukr.net>
2020-12-04 17:32:38 +0200

    HW3.5 optional testget() done

--- src/test/java/ru/javawebinar/topjava/MealTestData.java
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -3,8 +3,45 @@ package ru.javawebinar.topjava;
 import ru.javawebinar.topjava.matcher.BeanMatcher;
 import ru.javawebinar.topjava.model.Meal;
 
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+import static ru.javawebinar.topjava.model.BaseEntity.START_SEQ;
+
 public class MealTestData {
 
+    public static int MEAL_ID = START_SEQ + 2;
+
+    public static final List<Meal> MEALS = Arrays.asList(
+            new Meal(MEAL_ID++, LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500),
+            new Meal(MEAL_ID++, LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000),
+            new Meal(MEAL_ID++, LocalDateTime.of(2015, Month.MAY, 30, 20, 0), "Ужин", 500),
+            new Meal(MEAL_ID++, LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак", 500),
+            new Meal(MEAL_ID++, LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 1000),
+            new Meal(MEAL_ID++, LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510)
+    );
+
+//
+//            (100000, '2015-05-30 10:00:00', 'Завтрак', 500),
+//            (100000, '2015-05-30 13:00:00', 'Обед', 1000),
+//            (100000, '2015-05-30 20:00:00', 'Ужин', 500),
+//            (100000, '2015-05-31 10:00:00', 'Завтрак', 500),
+//            (100000, '2015-05-31 13:00:00', 'Обед', 1000),
+//            (100000, '2015-05-31 20:00:00', 'Ужин', 510),
+//            (100001, '2015-06-01 14:00:00', 'Админ ланч', 510),
+//            (100001, '2015-06-01 21:00:00', 'Админ ужин', 1500);
+//
+/*    public static final BeanMatcher<Meal> MATCHER = new BeanMatcher<>(
+            new Function<Meal, String>(){
+                @Override
+                public String apply(Meal meal){
+                    return meal.toString();
+                }
+            }
+);*/
     public static final BeanMatcher<Meal> MATCHER = new BeanMatcher<>();
 
 }
---
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -0,0 +1,72 @@
+package ru.javawebinar.topjava.service;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.bridge.SLF4JBridgeHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import ru.javawebinar.topjava.AuthorizedUser;
+import ru.javawebinar.topjava.MealTestData;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
+import ru.javawebinar.topjava.util.DbPopulator;
+
+import static org.junit.Assert.*;
+import static ru.javawebinar.topjava.MealTestData.*;
+
+@ContextConfiguration({
+        "classpath:spring/spring-app.xml",
+        "classpath:spring/spring-db.xml"
+})
+@RunWith(SpringRunner.class)
+public class MealServiceTest {
+
+    static {
+        SLF4JBridgeHandler.install();
+    }
+
+    @Autowired
+    private MealService service;
+
+    @Autowired
+    private DbPopulator dbPopulator;
+
+    @Before
+    public void setUp() throws Exception {
+        dbPopulator.execute();
+    }
+
+    @Test
+    public void get() {
+        for (Meal meal: MEALS) {
+            Meal testmeal = service.get(meal.getId(), AuthorizedUser.id());
+            MATCHER.assertEquals(meal, testmeal);
+        }
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void getBetweenDates() {
+    }
+
+    @Test
+    public void getBetweenDateTimes() {
+    }
+
+    @Test
+    public void getAll() {
+    }
+
+    @Test
+    public void update() {
+    }
+
+    @Test
+    public void save() {
+    }
+}
